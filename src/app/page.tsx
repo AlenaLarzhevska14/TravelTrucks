511783b6e7552da54ae75a5455d28b6593f3fa37
AlenaLarzhevska14 <alenna11358@gmail.com>
2026-07-03 23:19:12 +0300

    Add Hero and Header section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Header from '@/components/Header/Header';
+import Hero from '@/components/Hero/ Hero';
+
 export default function Home() {
   return (
-    <main>
-      <h1>Travel Trucks</h1>
-    </main>
+    <>
+      <Header />
+      <Hero />
+    </>
   );
 }
